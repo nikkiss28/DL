8ff0596ecb6e4bcd6400bf68b46282cc53e47432
nikkiss28 <thisisnikki28@gmail.com>
2025-05-08 22:47:22 +0530

    Update CNN_Hard.py

--- CNN_Hard.py
+++ CNN_Hard.py
@@ -1,45 +1,56 @@
-import cv2
-from google.colab.patches import cv2_imshow
-image=cv2.imread('imuu.png')
-cv2_imshow(image)
-gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-cv2_imshow(gray)
+import numpy as np
+image=np.array([
+    [1, 2, 3, 0, 1],
+    [0, 1, 2, 3, 1],
+    [1, 0, 2, 2, 0],
+    [2, 1, 0, 1, 2],
+    [0, 1, 3, 1, 0]
+])
 
-def convolution(image,filter,padding=0,stride=1):
-  img_h,img_w=image.shape
-  filt_h,filt_w=filter.shape
+kernel=np.array([
+    [1, 0, -1],
+    [1, 0, -1],
+    [1, 0, -1]
+])
 
-  if padding>0:
-    image=np.pad(image,((padding,padding),(padding,padding)),mode='constant',constant_values=0)
-  out_h=(image.shape[0]-filt_h)//stride+1
-  out_w=(image.shape[1]-filt_w)//stride+1
-  result=np.zeros((out_h,out_w))
+def convolution(image,kernel,stride=1):
+    img_h,img_w=image.shape
+    filt_h,filt_w=kernel.shape
+    out_h=(image.shape[0]-filt_h)//stride+1
+    out_w=(image.shape[1]-filt_w)//stride+1
+    result=np.zeros((out_h,out_w))
 
-  for i in range(out_h):
-    for j in range(out_w):
-      patch=image[i*stride:i*stride+filt_h,j*stride:j*stride+filt_w]
-      value=np.sum(patch*filter)
-      result[i,j]=max(0,value)
-  return result
+    for i in range(out_h):
+        for j in range(out_w):
+            patch=image[i*stride:i*stride+filt_h,j*stride:j*stride+filt_w]
+            result[i,j]=np.sum(patch*kernel)
+    return result
 
-def pooling(image,pool_size=2,stride=1):
-  img_h,img_w=image.shape
-  out_h=((img_h-pool_size))//stride+1
-  out_w=((img_w-pool_size))//stride+1
-  result=np.zeros((out_h,out_w))
+conv=convolution(image,kernel)
+print(conv)
 
-  for i in range(out_h):
-    for j in range(out_w):
-      patch=image[i*stride:i*stride+pool_size,j*stride:j*stride+pool_size]
-      value=np.sum(patch)
-      result[i,j]=np.max(patch)
-  return result
+def max_pooling(image,pool_size=2,stride=1):
+    img_h,img_w=image.shape
+    out_h=(img_h-pool_size)//stride+1
+    out_w=(img_w-pool_size)//stride+1
+    result=np.zeros((out_h,out_w))
+    for i in range(out_h):
+        for j in range(out_w):
+            patch=image[i*stride:i*stride+pool_size,j*stride:j*stride+pool_size]
+            result[i,j]=np.max(patch)
+    return result
 
-conv_img=convolution(gray,np.array([[0,-1,0],[-1,5,-1],[0,-1,0]]))
+pool=max_pooling(conv)
+print(pool)
 
-pool_img=pooling(conv_img)
-flattend=pool_img.flatten()
-weight=np.random.rand(flattend.shape[0])
-net=np.dot(flattend,weight)
-out=1/(1+np.exp(-net))
-print(out)
+flat=pool.flatten()
+print(flat)
+
+def fully_connected(flat_inp,weight,bias):
+    return np.dot(flat_inp,weights)+bias
+
+weights=np.random.rand(flat.size,2)
+bias=np.random.rand(2)
+
+fc=fully_connected(flat,weights,bias)
+print(fc)
