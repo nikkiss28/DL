5705e10e5d588a7791df980b9011e94a80aa87cc
nikkiss28 <thisisnikki28@gmail.com>
2025-05-08 23:14:44 +0530

    Update AutovsPCA_IRIS.py

--- AutovsPCA_IRIS.py
+++ AutovsPCA_IRIS.py
@@ -1,20 +1,46 @@
 from sklearn.datasets import load_iris
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from sklearn.decomposition import PCA
-from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
-iris = load_iris()
-dataset1 = StandardScaler().fit_transform(iris.data)
+data=load_iris()
+X=data.data
+X_scaled=StandardScaler().fit_transform(X)
 pca=PCA(n_components=2)
-principal_components=pca.fit_transform(dataset1)
-inverse_transform_data=pca.inverse_transform(principal_components)
-mse=mean_squared_error(dataset1,inverse_transform_data)
-print("Mean Squared Error for PCA & Inverse PCA is ",mse)
+X_pca=pca.fit_transform(X_scaled)
+X_recons_pca=pca.inverse_transform(X_pca)
+mse=mean_squared_error(X_scaled,X_recons_pca)
+print(mse)
 
-from sklearn.neural_network import MLPRegressor
-autoencoder=MLPRegressor(hidden_layer_sizes=(2024,2,2024),activation='relu',solver='adam',max_iter=10000000)
-autoencoder.fit(dataset1,dataset1)
-reconstructed_data=autoencoder.predict(dataset1)
-mse_auto=mean_squared_error(dataset1,reconstructed_data)
-print("auto mse",mse_auto)
+plt.figure(figsize=(6, 4))
+plt.scatter(X_pca[:,0], X_pca[:,1],cmap='viridis',c=data.target)
+plt.title("PCA (2D) Visualization")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+plt.colorbar()
+plt.show()
+
+autoencoder=Sequential([
+    Dense(2,activation='relu',input_shape=(4,)),
+    Dense(4,activation='linear')
+])
+
+autoencoder.compile(loss='mse',optimizer='adam')
+autoencoder.fit(X_scaled,X_scaled,epochs=100,batch_size=16,verbose=0)
+encoder=Sequential(autoencoder.layers[:1])
+X_encoded=encoder.predict(X_scaled)
+X_ae_recons=autoencoder.predict(X_scaled)
+mse_ae=mean_squared_error(X_scaled,X_ae_recons)
+print(mse_ae)
+
+plt.figure(figsize=(6, 4))
+plt.scatter(X_encoded[:, 0], X_encoded[:, 1], c=data.target, cmap='viridis')
+plt.title("PCA (2D) Visualization")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+plt.colorbar()
+plt.show()
